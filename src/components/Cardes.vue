fabcd37ffebb693b10fbbeb9e49a6a054ecf3f99
IVAN GABRIEL MAGALHÃES RODRIGUES <bielivan219@gmail.com>
2025-09-26 14:10:12 +0000

    cardes

---
+++ src/components/Cardes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card card-border bg-base-100 w-96">
+  <div class="card-body">
+    <h2 class="card-title">{{tarefa.nome}}</h2>
+    <p>{{tarefa.descricao}}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">{{tarefa.propriedade}}</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default{
+    name:"Card",
+    props:{
+        tarefa:{
+            id:Number,
+            nome:String,
+            descricao:String,
+            propriedade:String,
+            status:boolean,
+        }
+    }
+}
+</script>
